2f5243b0df4adac6886703fe81aa320cb7b7e7d5
Baris Sencan <baris.sncn@gmail.com>
2025-12-31 18:12:00 +0000

    feat: update downloads page and add certifications section
    - Add Licenses & Certifications section to homepage with all LinkedIn certs
    - Retire No$GBA for Mac, replace with links to modern emulators
    - Mark Intel Display Adapter Management Tool as unavailable
    - Update downloads page styling with discontinued badges
    - Improve Education section typography consistency

--- pages/downloads.vue
+++ pages/downloads.vue
@@ -1,28 +1,48 @@
 <template>
-  <v-expansion-panels>
-    <v-expansion-panel v-for="(item, i) in items" :key="i">
-      <v-expansion-panel-header>
-        <template #actions>
-          <v-icon>$expand</v-icon>
-        </template>
-        <v-row no-gutters align="center">
-          <v-col cols="auto">
-            <v-icon>mdi-download</v-icon>
-          </v-col>
-          <v-col class="ml-2">
-            {{ item.title }}
-          </v-col>
-        </v-row>
-      </v-expansion-panel-header>
-      <v-expansion-panel-content>
-        <v-btn color="primary" class="ma-2 white--text" @click="window.open(item.url)">
-          Download {{ item.version }}
-          <v-icon right dark>mdi-download</v-icon>
-        </v-btn>
-        <editor v-model="item.description" mode="viewer" :outline="false" :render-config="renderConfig" />
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-  </v-expansion-panels>
+  <div>
+    <v-alert type="info" border="left" class="mb-4">
+      <div class="text-body-2">
+        These are some old utilities I made years ago. Most are now discontinued, but I'm keeping them here for archival
+        purposes.
+      </div>
+    </v-alert>
+    <v-expansion-panels>
+      <v-expansion-panel v-for="(item, i) in items" :key="i">
+        <v-expansion-panel-header>
+          <template #actions>
+            <v-icon>$expand</v-icon>
+          </template>
+          <v-row no-gutters align="center">
+            <v-col cols="auto">
+              <v-icon>{{ item.icon || 'mdi-download' }}</v-icon>
+            </v-col>
+            <v-col class="ml-2">
+              {{ item.title }}
+              <v-chip v-if="item.isDiscontinued" x-small class="ml-2" color="grey" text-color="white">
+                Discontinued
+              </v-chip>
+            </v-col>
+          </v-row>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <v-btn
+            v-if="item.url && !item.isUnavailable"
+            color="primary"
+            class="ma-2 white--text"
+            @click="window.open(item.url)"
+          >
+            Download {{ item.version }}
+            <v-icon right dark>mdi-download</v-icon>
+          </v-btn>
+          <v-chip v-else-if="item.isUnavailable" color="error" class="ma-2" label>
+            <v-icon left>mdi-alert</v-icon>
+            Download Unavailable
+          </v-chip>
+          <editor v-model="item.description" mode="viewer" :outline="false" :render-config="renderConfig" />
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </div>
 </template>
 
 <script lang="ts">
@@ -43,29 +63,29 @@ export default Vue.extend({
     items: [
       {
         title: 'No$GBA for Mac',
-        url: 'http://adf.ly/1hbZH9',
-        version: 'Build 13',
-        description: `[No\\$GBA](http://www.nogba.com/) is originally a Windows only Nintendo DS & GBA emulator. This is a port that I made using the popular [Wineskin](http://wineskin.urgesoftware.com/) wrapper to run No$GBA via wine. You don't need to have Wineskin on your Mac for this port to work.
+        icon: 'mdi-archive-outline',
+        isDiscontinued: true,
+        description: `__This project has been retired.__
 
-### Frequently Asked Questions
+Back in the early 2010s, there weren't many good options for running Nintendo DS and GBA games on Mac. I created a Wineskin-wrapped port of [No\\$GBA](http://www.nogba.com/) that was downloaded over 160,000 times.
 
-__Q.__ Where are my save files located at?
+The Mac emulation scene has come a long way since then. If you're looking to play GBA or DS games on your Mac today, I recommend these modern alternatives:
 
-__A.__ They are inside the application package itself. Right click on the application, select "Show Package Contents." From there, double click "Save Files" shortcut. Your save files should be there.
+### For GBA
+- **[mGBA](https://mgba.io/)** — Native Apple Silicon support, excellent accuracy, actively maintained
+- **[OpenEmu](https://openemu.org/)** — Beautiful unified emulator with support for many systems including GBA
 
-__Q.__ How do I increase the screen size?
-
-__A.__ With build 13, you can now just drag the window corners or double click the window title to maximize it and the game area should stretch accordingly.
-
-__Q.__ How do I increase the running speed of the emulator?
-
-__A.__ By default, holding the "delete" button on your keyboard should mutiply the running speed by roughly 5.`,
+### For Nintendo DS
+- **[melonDS](https://melonds.kuribo64.net/)** — Native Mac builds, excellent DS emulation, Wi-Fi support
+- **[DeSmuME](https://desmume.org/)** — Long-standing DS emulator with Mac support
+- **[OpenEmu](https://openemu.org/)** — Also supports DS via the DeSmuME core`,
       },
       {
         title: 'Network Tuner',
         url: 'http://www.mediafire.com/?q3v9zqbqlgrs95e',
         version: '1.1.0',
-        description: `__This project is abandoned, meaning that no new versions will come.__
+        isDiscontinued: true,
+        description: `__This project is discontinued and no longer maintained.__
 
 _Does not run on PPC Macs. Requires Mac OS X 10.6 or later. On 10.8 app crashes after making optimizations, but they are applied successfully._
 
@@ -77,9 +97,12 @@ Network Tuner helps you tune your network performance by making small optimizati
       },
       {
         title: 'Intel Display Adapter Management Tool',
-        url: 'http://uploaded.net/file/oxp1v39d',
         version: '2.0.0',
-        description: `__This project is abandoned, meaning that no new versions will come.__
+        isDiscontinued: true,
+        isUnavailable: true,
+        description: `__This project is discontinued and no longer maintained.__
+
+_Download unavailable due to defunct hosting service._
 
 Intel Display Adapter Management Tool is a software written in C++/CLI to help Windows users with integrated Intel GPUs. It lets users to switch between Software Vertex Processing and Hardware Vertex Processing for specific executables which increases frame rates drastically if the machine has a good CPU.`,
       },
@@ -91,7 +114,7 @@ Intel Display Adapter Management Tool is a software written in C++/CLI to help W
       meta: [
         {
           name: 'Downloads',
-          content: 'Close-source freebies.',
+          content: 'Archived utilities and tools.',
         },
       ],
     };
